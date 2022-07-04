ab8a49505b048772e3e5134ff13e8ff4fe4fb4ed
Tanveer Singh <Tanveerfb@gmail.com>
2022-07-05 01:34:08 +1000

    update 5 july

--- src/Components/FriendList.js
+++ src/Components/FriendList.js
@@ -3,59 +3,65 @@ import { Container, Form, Button, ButtonGroup, Image } from "react-bootstrap";
 import { useFireContext } from "../Context";
 
 export default function FriendList({ uid }) {
-  const { user, getFriends, getProfilePicture, getDisplayName } =
-    useFireContext();
+  const { getFriends, getProfilePicture, getDisplayName } = useFireContext();
   const [friendsView, setfriendsView] = useState(false);
   const [friendslist, setfriendslist] = useState([]);
-  const [friendsdata, setfriendsdata] = useState([]);
+  const [friendsdata, setfriendsdata] = useState("");
+  const [loading, setloading] = useState(false);
 
-  async function handleFriends(e) {
+  function handleFriends(e) {
     e.preventDefault();
-    setfriendslist([]);
-    const data = await getFriends(uid);
-    setfriendsdata(data);
-    console.log(friendsdata);
-    friendsdata.forEach((fd) => {
-      getProfilePicture(fd).then((pp) => {
-        getDisplayName(fd).then((dn) => {
-          setfriendslist((friendslist) => [...friendslist, [pp, dn, fd]]);
+    setloading(true);
+    try {
+      setfriendslist([]);
+      getFriends(uid).then((friend) => {
+        // console.log(friend);
+        friend.map((f) => {
+          getDisplayName(f).then((dn) => {
+            getProfilePicture(f).then((pp) => {
+              setfriendslist((friendslist) => [...friendslist, [dn, pp, f]]);
+            });
+          });
         });
       });
-    });
-    setfriendsView(!friendsView);
+      setfriendsView(!friendsView);
+      setloading(false);
+    } catch (err) {
+      console.log(err);
+      setloading(false);
+    }
   }
 
   return (
     <>
-      <Container className="d-grid gap-2">
-        <Button variant="dark" onClick={handleFriends}>
+      <Container className="d-grid gap-2 mt-3">
+        <Button
+          disabled={loading}
+          id="getData"
+          variant={loading ? "secondary" : "dark"}
+          onClick={handleFriends}
+        >
           Friends
         </Button>
       </Container>
       {friendsView ? (
-        <>
-          <Container className="mt-4">
-            {friendslist.map((e) => {
-              return (
-                <Container className="d-flex justify-content-between borderLine p-2 my-2">
-                  <h3 className="mx-2">{e[1]}</h3>
-                  <ButtonGroup>
-                    <Button className="mx-2" variant="outline-dark">
-                      Message
-                    </Button>
-                    <Button
-                      className="mx-2"
-                      value={e[2]}
-                      variant="outline-dark"
-                    >
-                      UnFollow
-                    </Button>
-                  </ButtonGroup>
-                </Container>
-              );
-            })}
-          </Container>
-        </>
+        <Container className="mt-4">
+          {friendslist.map((e) => {
+            return (
+              <Container className="d-flex justify-content-between borderLine p-2 my-2">
+                <h3 className="mx-2">{e[0]}</h3>
+                <ButtonGroup>
+                  <Button className="mx-2" variant="outline-dark">
+                    Message
+                  </Button>
+                  <Button className="mx-2" value={e[2]} variant="outline-dark">
+                    UnFollow
+                  </Button>
+                </ButtonGroup>
+              </Container>
+            );
+          })}
+        </Container>
       ) : (
         ""
       )}
